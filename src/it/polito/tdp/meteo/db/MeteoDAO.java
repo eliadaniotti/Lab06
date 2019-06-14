fc93924d870bc4dd4b19d9928ef23a3b9fcc4220
Elia Daniotti <Elia Daniotti@DESKTOP-55HFKSK>
2019-06-14 19:10:05 +0200

    dao

--- src/it/polito/tdp/meteo/db/MeteoDAO.java
+++ src/it/polito/tdp/meteo/db/MeteoDAO.java
@@ -40,13 +40,55 @@ public class MeteoDAO {
 	}
 
 	public List<Rilevamento> getAllRilevamentiLocalitaMese(int mese, String localita) {
+		final String sql = "SELECT Localita, Data, Umidita FROM situazione WHERE localita=? AND DATA LIKE'2013-?%'";
 
-		return null;
+		List<Rilevamento> rilevamenti = new ArrayList<Rilevamento>();
+
+		try {
+			Connection conn = DBConnect.getInstance().getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, localita);
+			st.setInt(2, mese);
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+
+				Rilevamento r = new Rilevamento(rs.getString("Localita"), rs.getDate("Data"), rs.getInt("Umidita"));
+				rilevamenti.add(r);
+			}
+
+			conn.close();
+			return rilevamenti;
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 
 	public Double getAvgRilevamentiLocalitaMese(int mese, String localita) {
+		String sql = "SELECT AVG(umidita) FROM situazione WHERE localita=? AND DATA LIKE'2013-%'";
+		double media=0.0;
+		
+		try {
+			Connection conn = DBConnect.getInstance().getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, localita);
+			st.setInt(2, mese);
+			ResultSet rs = st.executeQuery();
 
-		return 0.0;
+			if (rs.next())
+				media=rs.getInt("umidita");
+
+			conn.close();
+			return media;
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 
 }
--- src/it/polito/tdp/meteo/db/TestMeteoDAO.java
+++ src/it/polito/tdp/meteo/db/TestMeteoDAO.java
@@ -20,8 +20,8 @@ public class TestMeteoDAO {
 //		System.out.println(dao.getAllRilevamentiLocalitaMese(1, "Genova"));
 //		System.out.println(dao.getAvgRilevamentiLocalitaMese(1, "Genova"));
 //		
-//		System.out.println(dao.getAllRilevamentiLocalitaMese(5, "Milano"));
-//		System.out.println(dao.getAvgRilevamentiLocalitaMese(5, "Milano"));
+		System.out.println(dao.getAllRilevamentiLocalitaMese(11, "Milano"));
+		System.out.println(dao.getAvgRilevamentiLocalitaMese(11, "Milano"));
 //		
 //		System.out.println(dao.getAllRilevamentiLocalitaMese(5, "Torino"));
 //		System.out.println(dao.getAvgRilevamentiLocalitaMese(5, "Torino"));
